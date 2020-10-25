b3d9db60baa8fc6a451a20be55db58495ff440b7
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2020-10-25 18:07:31 +0100

    build(package): add proper bulid and package workflow
    * add project scripts with invoke
    * add flake8 and black code linters

--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
 from setuptools import setup, find_packages
 
 # versioning is handled by bumpversion
-__version__ = '0.0.1'
+__version__ = "0.0.1"
+
 
 def get_file(filename):
     with open(filename) as f:
         return f.read()
 
+
 setup(
-    name='pelican-data-files',
+    name="pelican-data-files",
     description="Pelican plugin that allows to load data from files like JSON or YAML",
-    url='https://github.com/LucasVanHaaren/pelican-data-files',
-    author='vhash',
-    author_email='vanhaaren.lucas+dev@gmail.com',
-    keywords='pelican, pelican-plugin, data',
+    url="https://github.com/LucasVanHaaren/pelican-data-files",
+    author="vhash",
+    author_email="vanhaaren.lucas+dev@gmail.com",
+    keywords="pelican, pelican-plugin, data",
     version=__version__,
-    long_description=get_file('README.md'),
-    license=get_file('LICENSE'),
+    long_description=get_file("README.md"),
+    license=get_file("LICENSE"),
     packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=[
-        'pelican>=4.5'
-    ],
-    extras_require={
-        'dev': [
-            'bumpversion',
-            'flake8',
-            'black'
-        ]
-    }
+    python_requires=">=3.6",
+    install_requires=["pelican>=4.5"],
+    extras_require={"dev": ["invoke", "bumpversion", "flake8", "black"]},
 )
