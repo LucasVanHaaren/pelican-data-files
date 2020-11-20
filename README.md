fa76f6ba858aa2a3c5b7a5787dfd44efc7e7dee1
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2020-11-20 19:38:16 +0100

    build: first release on pypi
    * move package metadata to setup.cfg
    * add build and publish tasks
    * update README
    * version bump to 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# pelican-data-files
-Pelican plugin that allows to load data from files like JSON or YAML.
+<!-- markdownlint-disable MD033 -->
+<h1 align="center">
+  <br>
+    <img src="https://user-images.githubusercontent.com/29121316/99832308-a29d3a80-2b60-11eb-9e44-1ba3438bbe6b.png" alt="pelican-data-files-logo" width="700"></a>
+  <br>
+</h1>
+
+<!-- markdownlint-disable MD041 -->
+![pypi_badge](https://img.shields.io/pypi/v/pelican-data-files?logo=pypi&logoColor=yellow&style=for-the-badge)
+
+> Pelican plugin that allows to load data from files like JSON or YAML.
+
+This plugin loads all the data files in the `data /` directory, and makes them accessible via the context with the prefix `DATA_` followed by the name of the file. This is roughly the default behavior of the well known [Jekyll](https://jekyllrb.com/) static site generator.
+
+For example, the data of a `profile.json` file will be accessible from any template via `DATA_PROFILE`.
+
+You can use it directly on your personnal project, or to build a theme.
--- tasks.py
+++ tasks.py
@@ -41,3 +41,50 @@ def lint(c):
     """Run all linting tools"""
     black(c, check=True)
     flake8(c)
+
+
+@task
+def clean(c, dist=False, build=False):
+    """Clean build directories"""
+    patterns = []
+    if dist:
+        patterns.append("dist/")
+    if build:
+        patterns.append("build/")
+
+    for p in patterns:
+        c.run(f"rm -rf {p}")
+
+
+@task()
+def build(c, source=False, wheel=False, egg=False):
+    """Build source and wheel package"""
+    SF, WF, EF = "", "", ""
+    if source:
+        SF = "sdist"
+    if wheel:
+        WF = "bdist_wheel"
+    if egg:
+        WF = "bdist_egg"
+    if source or wheel or egg:
+        clean(c, dist=True, build=True)
+        return c.run(f"python setup.py {SF} {WF} {EF}", hide=True, warn=True)
+
+
+@task
+def buildcheck(c):
+    """Check if builds are OK"""
+    return c.run("twine check dist/*")
+
+
+@task
+def publish(c, prod=False):
+    """Publish package on PyPI"""
+    build_res = build(c, source=True, wheel=True)
+    if build_res.ok:
+        build_check_res = buildcheck(c)
+        if build_check_res.ok:
+            if prod:
+                c.run("twine upload dist/*")
+            else:
+                c.run("twine upload -r testpypi dist/*")
