ededf5b27f93b050a204fd537b04c2e170219e52
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2020-11-20 19:38:16 +0100

    build: first release on pypi
    * move package metadata to setup.cfg
    * add build and publish tasks
    * update README
    * version bump to 1.0.0

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
 from setuptools import setup
 
-# versioning is handled by bumpversion
-__version__ = "0.1.0"
-
-
-def get_file(filename):
-    with open(filename) as f:
-        return f.read()
-
-
-setup(
-    name="pelican-data-files",
-    description="Pelican plugin that allows to load data from files like JSON or YAML",
-    url="https://github.com/LucasVanHaaren/pelican-data-files",
-    author="vhash",
-    author_email="29121316+LucasVanHaaren@users.noreply.github.com",
-    keywords="pelican, pelican-plugin, data",
-    version=__version__,
-    long_description=get_file("README.md"),
-    license=get_file("LICENSE"),
-    packages=["pelican.plugins.data-files"],
-    python_requires=">=3.6",
-    install_requires=["pelican>=4.5"],
-    extras_require={"dev": ["invoke", "bumpversion", "flake8", "black"]},
-)
+setup()
