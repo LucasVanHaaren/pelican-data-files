0c72181123f74ec8fe4c14205b855cd30ad9fc55
Martin Folkers <hello@twobrain.io>
2022-09-24 09:40:56 +0200

    Add docblocks & type hints

--- pelican/plugins/data_files/__init__.py
+++ pelican/plugins/data_files/__init__.py
@@ -1,11 +1,24 @@
-from pelican import signals
+from pelican import Pelican, signals
 
 from .generator import DataGenerator
 
 
-def get_generators(pelican_object):
+def get_generators(pelican_object: Pelican):
+    """
+    Provides custom generator
+
+    :param pelican_object: Pelican
+    :return: DataGenerator
+    """
+
     return DataGenerator
 
 
-def register():
+def register() -> None:
+    """
+    Registers custom generator
+
+    :return: None
+    """
+
     signals.get_generators.connect(get_generators)
